5894c317c48efc6513271af6feb89e7934af95a3
soberjan1 <aliguseynov48@gmail.com>
2023-04-30 00:55:09 +0300

    task 1
    for mentor check

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// РАМЗЕТКА
+const galleryItemsEl = document.querySelector(".gallery");
+const addGalleryItems = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+  )
+  .join("");
+galleryItemsEl.insertAdjacentHTML("afterbegin", addGalleryItems);
+
+// ДИЛЕГИРОВАНИЕ
+galleryItemsEl.addEventListener("click", bigImg);
+function bigImg(evt) {
+  evt.preventDefault();
+  if (evt.target.classList.contains(".gallery__image")) {
+    return evt.tagret.dataset.source;
+  }
+}
+
+// БИБЛИОТЕКА basiclightbox
+const instance = basicLightbox.create(`
+    <img src="${evt.tagret.dataset.source}" width="800" height="600">
+`);
+
+instance.show();
