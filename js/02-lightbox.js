1cbe9c03fce9c418c2e68793b9c68e09da0f5a57
soberjan1 <aliguseynov48@gmail.com>
2023-04-30 23:42:38 +0300

    task 2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// РАЗМЕТКА
+const galleryItemsEl = document.querySelector(".gallery");
+const addGalleryItems = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+  )
+  .join("");
+galleryItemsEl.insertAdjacentHTML("afterbegin", addGalleryItems);
+
+// БИБЛИОТЕКА SimpleLightbox
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
